30417b29a814a6c1121e8b4373ed9a310e5bc86d
zyronon <zyronon@163.com>
2023-08-31 01:03:36 +0800

    添加文章

--- src/components/TypeArticle.vue
+++ src/components/TypeArticle.vue
@@ -16,55 +16,65 @@ INVESTOR'S CHRONICLE, March 23 1990`
 // article1 = `Last week I went to the theatre.\nI had a very good seat. The play was very interesting. I did not enjoy it. A young man and a young woman were sitting behind me. They were talking loudly. I got very angry. I could not hear the actors. I turned round. I looked at the man and the woman angrily. They did not pay any attention. In the end, I could not bear it. I turned round again. I cant hear a word! I said angrily.
 // Its none of your business, the young man said rudely. This is a private conversation!`
 
+let article2 = `What is one of the features of modern camping where nationality is concerned?
+Economy is one powerful motive for camping, since after the initial outlay upon equipment, or through hiring it, the total expense can be far less than the cost of hotels. But, contrary to a popular assumption, it is far from being the only one, or even the greatest. The man who manoeuvres carelessly into his twenty pounds' worth of space at one of Europe's myriad permanent sites may find himself bumping a Bentley. More likely, Ford Escort will be hub to hub with Renault or Mercedes, but rarely with bicycles made for two.
+That the equipment of modern camping becomes yearly more sophisticated is an entertaining paradox for the cynic, a brighter promise for the hopeful traveler who has sworn to get away from it all. It also provides-and some student sociologist might care to base his thesis upon the phenomenon -- an escape of another kind. The modern traveller is often a man who dislikes the Splendide and the Bellavista, not because he cannot afford, or shuns their material comforts. but because he is afford of them. Affluent he may be, but he is by no means sure what to tip the doorman or the chambermaid. Master in his own house, he has little idea of when to say boo to a maitre d'hotel.
+From all such fears camping releases him. Granted, a snobbery of camping itself, based upon equipment and techniques, already exists; but it is of a kind that, if he meets it, he can readily understand and deal with. There is no superior 'they' in the shape of managements and hotel hierarchies to darken his holiday days.
+To such motives, yet another must be added. The contemporary phenomenon of car worship is to be explained not least by the sense of independence and freedom that ownership entails. To this pleasure camping gives an exquisite refinement.
+From one's own front door to home or foreign hills or sands and back again, everything is to hand. Not only are the means of arriving at the holiday paradise entirely within one's own command and keeping, but the means of escape from holiday hel (if the beach proves too crowded, the local weather too inclement) are there, outside -- or, as likely, part of -- the tent.
+Idealists have objected to the package tour, that the traveller abroad thereby denies himself the opportunity of getting to know the people of the country visited. Insularity and self-containment, it is argued, go hand in hand. The opinion does not survive experience of a popular Continental camping place. Holiday hotels tend to cater for one nationality of visitors especially, sometimes exclusively. Camping sites, by contrast, are highly cosmopolitan. Granted, a preponderance of Germans is a characteristic that seems common to most Mediterranean sites; but as yet there is no overwhelmingly specialized patronage. Notices forbidding the open-air drying of clothes, or the use of water points for car washing, or those inviting 'our camping friends' to a dance or a boat trip are printed not only in French or Italian or Spanish, but also in English, German and Dutch. At meal times the odour of sauerkraut vies with that of garlic. The Frenchman's breakfast coffee competes with the Englishman's bacon and eggs.
+Whether the remarkable growth of organized camping means the eventual death of the more independent kind is hard to say. Municipalities naturally want to secure the campers' site fees and other custom. Police are wary of itinerants who cannot be traced to a recognized camp boundary or to four walls. But most probably it will all depend upon campers themselves: how many heath fires they cause; how much litter they leave; in short, whether or not they wholly alienate landowners and those who live in the countryside. Only good scouting is likely to preserve the freedoms so dear to the heart of the eternal Boy Scout.
+NIGEL BUXTON The Great Escape from The Weekend Telegraph`
+
 let isPlay = $ref(false)
 let inputRef = $ref<HTMLInputElement>(null)
 
 const [playAudio] = usePlayWordAudio()
 
 interface Article {
-  section: {
+  sections: {
     sentence: string,
     words: string[]
   }[][]
 }
 
 let article = reactive<Article>({
-  section: []
+  sections: []
 })
 
 onMounted(() => {
-  let sections = article1.split('\n')
+  let sections = article2.split('\n')
   sections = sections.reduce((previousValue: any[], currentValue, currentIndex, array) => {
-    console.log(cloneDeep(currentValue))
+    // console.log(cloneDeep(currentValue))
     let sentences = currentValue.replace('\n', '').split('.')
-    console.log(cloneDeep(sentences))
+    // console.log(cloneDeep(sentences))
     sentences = sentences.filter(v => v).map((v, index, arr) => {
       v += index === arr.length - 1 ? '' : '.'
       return v
     })
-    console.log(cloneDeep(sentences))
+    // console.log(cloneDeep(sentences))
 
     sentences = sentences.reduce((previousValue: string[], currentValue: string, currentIndex, array) => {
       let r = currentValue.split(',')
-      r = r.map((v, index) => {
+      r = r.filter(v => v).map((v, index) => {
         v += index === r.length - 1 ? '' : ','
         return v
       })
       previousValue = previousValue.concat(r)
       return previousValue
     }, [])
-    console.log(cloneDeep(sentences))
+    // console.log(cloneDeep(sentences))
 
     sentences = sentences.reduce((previousValue: string[], currentValue: string, currentIndex, array) => {
       let r = currentValue.split('?')
-      r = r.map((v, index) => {
+      r = r.filter(v => v).map((v, index) => {
         v += index === r.length - 1 ? '' : '?'
         return v
       })
       previousValue = previousValue.concat(r)
       return previousValue
     }, [])
-    console.log(cloneDeep(sentences))
+    // console.log(cloneDeep(sentences))
 
 
     let section = sentences.map(v => {
@@ -81,6 +91,7 @@ onMounted(() => {
   }, [])
 
   console.log(sections)
+  article.sections = sections as any
   // console.log(cloneDeep(item.sentences))
 })
 
@@ -104,8 +115,70 @@ function focus() {
   inputRef.focus()
 }
 
+let sectionIndex = $ref(0)
+let sentenceIndex = $ref(0)
+let wordIndex = $ref(0)
+let index = $ref(0)
+let input = $ref('')
+let wrong = $ref('')
+let isSpace = $ref(false)
+
 function keyDown(e: KeyboardEvent) {
-  console.log('keyDown', e.key)
+  console.log('keyDown', e.key, e.code)
+  if (isSpace) {
+    if (e.code === 'Space') isSpace = false
+    return
+  }
+  if ((e.keyCode >= 65 && e.keyCode <= 90)
+      || e.code === 'Space'
+      || e.code === 'Slash'
+      || e.code === 'Quote'
+      || e.code === 'Comma'
+      || e.code === 'BracketLeft'
+      || e.code === 'BracketRight'
+  ) {
+    let letter = e.key
+
+    let currentSection = article.sections[sectionIndex]
+    let currentSentence = currentSection[sentenceIndex]
+    let currentWord = currentSentence.words[wordIndex]
+    let key = currentWord[index]
+    if (key === letter) {
+      input += letter
+      wrong = ''
+      console.log('匹配上了')
+      index++
+      if (!currentWord[index]) {
+        index = 0
+        input = wrong = ''
+        wordIndex++
+        isSpace = true
+        if (!currentSentence.words[wordIndex]) {
+          wordIndex = 0
+          sentenceIndex++
+          isSpace = true
+          if (!currentSection[sentenceIndex]) {
+            sentenceIndex = 0
+            sectionIndex++
+          }
+        }
+      }
+    } else {
+      wrong = letter
+      setTimeout(() => {
+        wrong = ''
+        // wrong = input = ''
+      }, 500)
+      console.log('未匹配')
+    }
+  }
+
+  console.log(
+      'sectionIndex', sectionIndex,
+      'sentenceIndex', sentenceIndex,
+      'wordIndex', wordIndex,
+      'index', index,
+  )
   inputRef.value = ''
   e.preventDefault()
 }
@@ -114,12 +187,34 @@ function keyDown(e: KeyboardEvent) {
 <template>
   <div class="type-wrapper">
     <article v-if="true" @click="focus">
-      <span v-for="i in article.sentences" @click="playAudio(i)">
-        <span class="word" v-for="word in i.words">{{ word }}</span>
-      </span>
+      <p class="line" v-for="(section,indexI) in article.sections">
+        <span v-for="(sentences,indexJ) in section">
+          <span class="word"
+                v-for="(word,indexW) in sentences.words"
+                :class="(sectionIndex>indexI
+                ?'green':
+                (sectionIndex>=indexI &&sentenceIndex>indexJ)
+                ?'green' :
+                (sectionIndex>=indexI &&sentenceIndex>=indexJ && wordIndex>indexW)
+                ?'green':
+                'wait'
+                )"
+          >
+            <template v-if="(sectionIndex===indexI &&sentenceIndex===indexJ && wordIndex===indexW)">
+                 <span class="input" v-if="input">{{ input }}</span>
+                <span class="wrong"
+                      :class="wrong === ' ' && 'bg-wrong'"
+                      v-if="wrong">{{ wrong === ' ' ? `&nbsp;` : wrong }}</span>{{
+                word.slice(input.length + wrong.length)
+              }}&nbsp;</template>
+            <template v-else>{{ word }}&nbsp;</template>
+            <!--          <span>{{sentences.sentence}}&nbsp;</span>-->
+          </span>
+        </span>
+      </p>
     </article>
     <input ref="inputRef"
-           class="input"
+           class="inputEl"
            type="text"
            @keydown="keyDown">
   </div>
@@ -127,6 +222,10 @@ function keyDown(e: KeyboardEvent) {
 
 <style scoped lang="scss">
 @import "@/assets/css/style.scss";
+
+.green {
+  color: green;
+}
 
 .type-wrapper {
   height: 100%;
@@ -138,7 +237,13 @@ function keyDown(e: KeyboardEvent) {
     font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace;
     letter-spacing: 0rem;
     color: gray;
-    white-space: pre-line;
+
+    .line {
+      //white-space: pre-line;
+      word-break: keep-all;
+      word-wrap: break-word;
+      white-space: pre-wrap;
+    }
 
     span {
       //background: red;
@@ -146,11 +251,23 @@ function keyDown(e: KeyboardEvent) {
     }
 
     .word {
-      margin-right: 10rem;
+      //margin-right: 10rem;
     }
   }
 
+
   .input {
+    color: rgb(22, 163, 74);
+  }
+
+  .wrong {
+    color: rgba(red, 0.6);
+  }
+  .bg-wrong {
+    background: rgba(red, 0.6);
+  }
+
+  .inputEl {
     position: fixed;
     left: 100vw;
   }
