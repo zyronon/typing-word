af6b5c1dd2c17feba111d7f0e877ff472a7469bd
zyronon <zyronon@163.com>
2023-10-15 02:30:33 +0800

    update README.md

---
+++ README.md
@@ -0,0 +1,103 @@
+<h1 align="center">
+  Typing Word
+</h1>
+
+[//]: # (<p align="center">)
+
+[//]: # (  <a href="./docs/README_EN.md">English</a>)
+
+[//]: # (</p>)
+
+<p align="center">
+  可在网页上使用的背单词软件
+</p>
+
+<p align="center">
+  <a href="https://github.com/zyronon/type-word/blob/master/LICENSE"><img src="https://img.shields.io/github/license/zyronon/type-word" alt="License"></a>
+  <a><img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg"/></a>
+  <a><img src="https://img.shields.io/badge/Powered%20by-Vue-blue"/></a>
+</p>
+
+[//]: # (<div align=center>)
+
+[//]: # (<img  src="docs/Screenshot.png"/>)
+
+[//]: # (</div>)
+
+## 📸 在线访问
+
+Netlify: <https://typing-words.netlify.app/>
+
+
+## 🛠 功能列表
+
+### 词库
+
+内置了常用的 CET-4 、CET-6 、GMAT 、GRE 、IELTS 、SAT 、TOEFL 、考研英语、专业四级英语、专业八级英语，也有程序员常见英语单词以及多种编程语言
+API 等词库。 尽可能满足大部分用户对单词记忆的需求，也非常欢迎社区贡献更多的词库。
+
+### 音标显示、发音功能
+
+方便用户在记忆单词时，同时记忆读音与音标。
+
+### 默写模式
+
+在用户完成一个章节的练习后，会弹出选项是否默写本章，方便用户巩固本章学习的单词。
+
+### 速度、正确率显示
+
+量化用户输入的速度和输入的正确率，让用户有感知的了解自己技能的提升
+
+## 运行项目
+
+本项目是基于`Vue`开发的，需要 node 环境来运行。
+
+### 手动安装
+
+1. 安装 NodeJS，参考[官方文档](https://nodejs.org/en/download)
+2. 使用 `git clone` 下载项目到本地, 不使用 git 可能因为缺少依赖而无法运行
+3. 打开命令行，在项目根目录下，运行`pnpm install`来下载依赖。
+4. 执行`pnpm start`来启动项目，项目默认地址为[`http://localhost:3000`](http://localhost:3000)
+5. 在浏览器中打开[`http://localhost:3000`](http://localhost:3000)  来访问项目。
+
+## 📕 词库列表
+
+- CET-4
+- CET-6
+- GMAT
+- GRE
+- IELTS
+- SAT
+- TOEFL
+- 考研英语
+- 专业四级英语
+- 专业八级英语
+- Coder Dict 程序员常用词
+- 高考
+- 中考
+- 商务英语
+- BEC
+- 人教版英语 3-9 年级
+- 王陆雅思王听力语料库 [@Saigyouji_WKKun](https://github.com/ggehuliang)
+- 日语常见词、N1 ～ N5 [@xiaojia](https://github.com/wetery)
+
+如果您需要背诵其他词库，欢迎在 Issue 中提出
+
+## 📗 API 词库
+
+- JavaScript API. [@sdu-gyf](https://github.com/sdu-gyf)
+- Node.js API. [@chrysalis1215](https://github.com/chrysalis1215)
+- Java API. [@darkSheep](https://github.com/SFAfreshman)
+- Linux Command. [@归谜](https://github.com/vhxubo)
+- C#: List API [@nidbCN](https://github.com/nidbCN)
+
+## 🎙 功能与建议
+
+目前项目处于开发初期，新功能正在持续添加中，如果你对软件有任何功能与建议，欢迎在 Issues 中提出
+如果你也喜欢本软件的设计思想，欢迎提交 pr，非常感谢你对我们的支持！
+
+### 灵感来源
+
+[qwerty-learner](https://github.com/RealKai42/qwerty-learner/)
+<br/><br/>
+ 
--- src/hooks/article.ts
+++ src/hooks/article.ts
@@ -33,7 +33,7 @@ export function splitEnArticle(text: string): { sections: Sentence[][], newText:
   // console.log(text)
   // console.time()
   let keyboardMap = EnKeyboardMap
-  // text = "On Jan. 20, former Sen. Barack Obama became the 44th President of the U.S. Millions attended the Inauguration. It was Sunday? I never get up early on Sundays! I sometimes stay in bed until lunchtime. Last Sunday I got up very late. I looked out of the window. It was dark outside. 'What a day!' I thought. 'It's raining again.' Just then, the telephone rang. It was my aunt Lucy. 'I've just arrived by train,' she said. 'I'm coming to see you.'\n    'But I'm still having breakfast,' I said\n    'What are you doing?' she asked.\n    'I'm having breakfast,' I repeated.\n    'Dear me,' she said. 'Do you always get up so late? It's one o'clock'"
+  // text = "On Jan. 20, former Sen. Barack Obama became the 44th President of the U.S. Millions attended the Inauguration. It was Sunday? I never get up early on Sundays! I sometimes stay in bed until lunchtime. Last Sunday I got up very late. I looked out of the window. It was dark outside. 'What a day!' I thought. 'It'LICENSE raining again.' Just then, the telephone rang. It was my aunt Lucy. 'I've just arrived by train,' she said. 'I'm coming to see you.'\n    'But I'm still having breakfast,' I said\n    'What are you doing?' she asked.\n    'I'm having breakfast,' I repeated.\n    'Dear me,' she said. 'Do you always get up so late? It'LICENSE one o'clock'"
   // text = "Mr.James Scott has a garage in Silbury and now he has just bought another garage in Pinhurst. Pinhurst is only five miles from Silbury, but Mr. Scott cannot get a telephone for his new garage, so he has just bought twelve pigeons. Yesterday, a pigeon carried the first message from Pinhurst to Silbury. The bird covered the distance in three minutes. Up to now, Mr.Scott has sent a great many requests for spare parts and other urgent messages from one garage to the other. In this way, he has begun his own private 'telephone' service."
   // text = "How does the older investor differ in his approach to investment from the younger investor?\nThere is no shortage of tipsters around offering 'get-rich-quick' opportunities. But if you are a serious private investor, leave the Las Vegas mentality to those with money to fritter. The serious investor needs a proper 'portfolio' -- a well-planned selection of investments, with a definite structure and a clear aim. But exactly how does a newcomer to the stock market go about achieving that?\nWell, if you go to five reputable stock brokers and ask them what you should do with your money, you're likely to get five different answers, -- even if you give all the relevant information about your age age, family, finances and what you want from your investments. Moral? There is no one 'right' way to structure a portfolio. However, there are undoubtedly some wrong ways, and you can be sure that none of our five advisers would have suggested sinking all (or perhaps any) of your money into Periwigs*.\nSo what should you do? We'll assume that you have sorted out the basics -- like mortgages, pensions, insurance and access to sufficient cash reserves. You should then establish your own individual aims. These are partly a matter of personal circumstances, partly a matter of psychology.\nFor instance, if you are older you have less time to recover from any major losses, and you may well wish to boost your pension income. So preserving your capital and generating extra income are your main priorities. In this case, you'd probably construct a portfolio with some shares (but not high risk ones), along with gilts, cash deposits, and perhaps convertibles or the income shares of split capital investment trusts.\nIf you are younger, and in a solid financial position, you may decide to take an aggressive approach -- but only if you're blessed with a sanguine disposition and won't suffer sleepless nights over share prices. If portfolio, alongside your more pedestrian in vestments. Once you have decided on your investment aims, you can then decide where to put your money. The golden rule here is spread your risk -- if you put all of your money into Periwigs International, you're setting yourself up as a hostage to fortune.\n*'Periwigs' is the name of a fictitious company.\nINVESTOR'S CHRONICLE, March 23 1990"
 
@@ -92,7 +92,7 @@ export function splitEnArticle(text: string): { sections: Sentence[][], newText:
           if (addCurrent) {
             sentence.words.push(word3)
           } else {
-            // 'Do you always get up so late? It's one o'clock!' 会被断成两句
+            // 'Do you always get up so late? It'LICENSE one o'clock!' 会被断成两句
             let lastSentence = section[section.length - 2]
             lastSentence.words = lastSentence.words.concat(sentence.words)
             lastSentence.words.push(word3)
@@ -222,7 +222,7 @@ export function splitCNArticle(text: string): Sentence[][] {
         words: []
       }
       section.push(sentence)
-      // console.log('s', )
+      // console.log('LICENSE', )
       if (row) {
         //这个库总是会把反引号给断句到下一行
         if (row[0] === "”") {
