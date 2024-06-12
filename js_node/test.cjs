0a6ccda5812802477f9043cbfd76f36d7113929e
王念超 <@163.com>
2024-06-12 18:35:34 +0800

    feat: update dict

--- js_node/test.cjs
+++ js_node/test.cjs
@@ -2,6 +2,40 @@ let path = require("path");
 let fs = require("fs");
 const axios = require('axios')
 
-let str = fs.readFileSync('./save/allNew.min.json', "utf8");
+let str = fs.readFileSync('./failWord.txt', "utf8");
+let failList = []
+let list = JSON.parse(str)
 
-console.log('str',JSON.parse(str)[0])
+async function sleep(val) {
+  return new Promise(resolve => {
+    setTimeout(resolve, val)
+  })
+}
+
+function fail(word) {
+  failList.push(word)
+  console.log('失败：', failList.length)
+  fs.writeFileSync('./fail.txt', JSON.stringify(failList, null, 2));
+}
+
+async function test() {
+  for (let i = 0; i < list.length; i++) {
+    let v = list[i]
+    console.log('进度', i)
+    await sleep(100)
+    axios({
+      url: 'http://localhost/index.php/v1/support/test',
+      method: 'post',
+      data: v
+    }).then(r => {
+      if (!r.data.success) {
+        fail(v.word)
+      }
+    }).catch(r => {
+      fail(v.word)
+    })
+  }
+}
+
+
+test()
---
+++ js_node/从原始词典提取单词.cjs
@@ -0,0 +1,35 @@
+let path = require("path");
+let fs = require("fs");
+
+let read = './dict/'
+let save = "./res/";
+
+//判断是不是目录
+const dirs = fs.readdirSync(read)
+dirs.forEach(dictName => {
+  formatDict(read, dictName)
+})
+
+// formatDict(read, 'BEC_2.json')
+
+
+function formatDict(path, name) {
+  try {
+    let newObj = []
+    let str = fs.readFileSync(path + name, "utf8");
+    let list = str.split('\n')
+    list.map(v => {
+      if (!v) return
+      let item = JSON.parse(v)
+      // console.log('v', item.headWord)
+      newObj.push(item.headWord)
+    })
+
+    // fs.writeFileSync(save + name, JSON.stringify(newObj, null, 2));
+    fs.writeFileSync(save + name.replace('.json', '_word.json'), JSON.stringify(newObj));
+    console.log(name, newObj.length)
+  } catch (e) {
+    console.log('err', name, e)
+  }
+}
+
