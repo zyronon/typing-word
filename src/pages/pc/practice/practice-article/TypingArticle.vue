47d4958a6a645205d45d82e88adf203c87b4c599
zyronon <zyronon@163.com>
2025-03-22 03:03:02 +0800

    feat: 调整文章编辑器

--- src/pages/pc/practice/practice-article/TypingArticle.vue
+++ src/pages/pc/practice/practice-article/TypingArticle.vue
@@ -9,7 +9,10 @@ import {cloneDeep} from "lodash-es";
 import {emitter, EventKey} from "@/utils/eventBus.ts";
 import jq from 'jquery'
 import {_nextTick} from "@/utils";
-
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
+import ContextMenu from '@imengyu/vue3-context-menu'
+import {useToast} from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
 interface IProps {
   article: Article,
@@ -52,13 +55,17 @@ let hoverIndex = $ref({
   sectionIndex: -1,
   sentenceIndex: -1,
 })
-let cursor = $ref({})
+let cursor = $ref({
+  top: 0,
+  left: 0,
+})
 let isEnd = $ref(false)
 
 const currentIndex = computed(() => {
   return `${sectionIndex}${sentenceIndex}${wordIndex}`
 })
 
+const $toast = useToast();
 const playBeep = usePlayBeep()
 const playCorrect = usePlayCorrect()
 const playKeyboardAudio = usePlayKeyboardAudio()
@@ -67,9 +74,6 @@ const playWordAudio = usePlayWordAudio()
 const store = useBaseStore()
 const statisticsStore = usePracticeStore()
 const settingStore = useSettingStore()
-
-//当跳过句子时，会同时触发光标检测和布局检测，但布局检测
-let isDelayCheckCursorPosition = false
 
 window.$ = jq
 watch([() => sectionIndex, () => sentenceIndex, () => wordIndex, () => stringIndex], ([a, b, c,]) => {
@@ -82,26 +86,16 @@ watch(() => props.article, () => {
   wordIndex = props.wordIndex
   stringIndex = props.stringIndex
   typeArticleRef?.scrollTo({top: 0, behavior: "smooth"})
-  checkTranslateLocation()
-  checkCursorPosition()
+  checkTranslateLocation().then(() => checkCursorPosition())
 }, {immediate: true})
 
-watch(() => settingStore.dictation, () => {
-  if (settingStore.translate) {
-    checkTranslateLocation()
-  }
-  isDelayCheckCursorPosition = true
-  checkCursorPosition()
-})
 watch(() => settingStore.translate, () => {
-  isDelayCheckCursorPosition = true
-  checkCursorPosition()
-  checkTranslateLocation()
+  checkTranslateLocation().then(() => checkCursorPosition())
 })
 
 
 function checkCursorPosition(a = sectionIndex, b = sentenceIndex, c = wordIndex) {
-  console.log('checkCursorPosition', isDelayCheckCursorPosition)
+  console.log('checkCursorPosition')
   _nextTick(() => {
     let currentWord = jq(`.section:nth(${a}) .sentence:nth(${b}) .word:nth(${c})`)
     // console.log(a, b, c, currentWord)
@@ -117,33 +111,34 @@ function checkCursorPosition(a = sectionIndex, b = sentenceIndex, c = wordIndex)
         // console.log(cursor)
       }
     }
-    isDelayCheckCursorPosition = false
-  }, isDelayCheckCursorPosition ? 300 : 0)
+  },)
 }
 
 function checkTranslateLocation() {
   console.log('checkTranslateLocation')
-  _nextTick(() => {
-    let articleRect = articleWrapperRef.getBoundingClientRect()
-    props.article.sections.map((v, i) => {
-      v.map((w, j) => {
-        let location = i + '-' + j
-        let wordClassName = `.word${location}`
-        let word = document.querySelector(wordClassName)
-        let wordRect = word.getBoundingClientRect()
-        let translateClassName = `.translate${location}`
-        let translate: HTMLDivElement = document.querySelector(translateClassName)
+  return new Promise<void>(resolve => {
+    _nextTick(() => {
+      let articleRect = articleWrapperRef.getBoundingClientRect()
+      props.article.sections.map((v, i) => {
+        v.map((w, j) => {
+          let location = i + '-' + j
+          let wordClassName = `.word${location}`
+          let word = document.querySelector(wordClassName)
+          let wordRect = word.getBoundingClientRect()
+          let translateClassName = `.translate${location}`
+          let translate: HTMLDivElement = document.querySelector(translateClassName)
 
-        translate.style.opacity = '1'
-        translate.style.top = wordRect.top - articleRect.top - 22 + 'px'
-        // @ts-ignore
-        translate.firstChild.style.width = wordRect.left - articleRect.left + 'px'
-        // console.log(word, wordRect.left - articleRect.left)
-        // console.log('word-wordRect', wordRect)
+          translate.style.opacity = '1'
+          translate.style.top = wordRect.top - articleRect.top + 28 + 'px'
+          // @ts-ignore
+          translate.firstChild.style.width = wordRect.left - articleRect.left + 'px'
+          // console.log(word, wordRect.left - articleRect.left)
+          // console.log('word-wordRect', wordRect)
+        })
       })
-    })
-    // checkCursorPosition(sectionIndex, sentenceIndex, wordIndex)
-  }, 300)
+      resolve()
+    }, 300)
+  })
 }
 
 
@@ -180,12 +175,6 @@ function nextSentence() {
       emit('over')
     }
   } else {
-    if (settingStore.dictation){
-      isDelayCheckCursorPosition = true
-    }
-    if (settingStore.dictation && settingStore.translate) {
-      checkTranslateLocation()
-    }
     playWordAudio(currentSection[sentenceIndex].text)
   }
   lockNextSentence = false
@@ -269,7 +258,6 @@ function onTyping(e: KeyboardEvent) {
   e.preventDefault()
 }
 
-
 function play() {
   let currentSection = props.article.sections[sectionIndex]
 
@@ -300,48 +288,12 @@ function playWord(word: ArticleWord) {
   playWordAudio(word.word)
 }
 
-function currentWordString(word: ArticleWord, i: number, i2: number,) {
-  let str = word.word.slice(input.length + wrong.length, input.length + wrong.length + 1)
-  if (word.isSymbol) {
-    return str
-  }
-  if (hoverIndex.sectionIndex === i && hoverIndex.sentenceIndex === i2) {
-    return str
-  }
-
-  if (settingStore.dictation) {
-    return '_'
-  }
-  return str
+function indexWord(word: ArticleWord) {
+  return word.word.slice(input.length, input.length + 1)
 }
 
-function currentWordEnd(word: ArticleWord, i: number, i2: number,) {
-  let str = word.word.slice(input.length + wrong.length + (wrong ? 0 : 1))
-  if (hoverIndex.sectionIndex === i && hoverIndex.sentenceIndex === i2) {
-    return str
-  }
-
-  if (settingStore.dictation) {
-    return str.split('').map(v => '_').join('')
-  }
-  return str
-}
-
-function getAllWord(word: ArticleWord, i: number, i2: number, i3: number) {
-  let str = word.word
-  if (word.isSymbol) {
-    return str
-  }
-
-  if (hoverIndex.sectionIndex === i && hoverIndex.sentenceIndex === i2) {
-    return str
-  }
-
-  //剩100是因为，可能存在特殊情况，比如003,010这种，0 12 24，100
-  if (sectionIndex * 10000 + sentenceIndex * 100 + wordIndex < i * 10000 + i2 * 100 + i3 && settingStore.dictation) {
-    return str.split('').map(v => '_').join('')
-  }
-  return str
+function remainderWord(word: ArticleWord,) {
+  return word.word.slice(input.length + 1)
 }
 
 function showSentence(i1: number = sectionIndex, i2: number = sentenceIndex) {
@@ -350,6 +302,54 @@ function showSentence(i1: number = sectionIndex, i2: number = sentenceIndex) {
 
 function hideSentence() {
   hoverIndex = {sectionIndex: -1, sentenceIndex: -1}
+}
+
+function onContextMenu(e: MouseEvent, text: string, i, j) {
+  //prevent the browser's default menu
+  e.preventDefault();
+  //show your menu
+  ContextMenu.showContextMenu({
+    x: e.x,
+    y: e.y,
+    items: [
+      {
+        label: "从这开始",
+        onClick: () => {
+          sectionIndex = i
+          sentenceIndex = j
+          playWordAudio(text)
+        }
+      },
+      {
+        label: "播放",
+        onClick: () => {
+          playWordAudio(text)
+        }
+      },
+      {
+        label: "复制",
+        onClick: () => {
+          navigator.clipboard.writeText(text).then(r => {
+            $toast.success('已复制！', {position: 'top'});
+          })
+        }
+      },
+      {
+        label: "语法分析",
+        onClick: () => {
+          navigator.clipboard.writeText(text).then(r => {
+            $toast.success('已复制！随后将打开语法分析网站！', {
+              position: 'top',
+              duration: 3000,
+            });
+            setTimeout(() => {
+              window.open('https://enpuz.com/')
+            }, 1000)
+          })
+        }
+      },
+    ]
+  });
 }
 
 onMounted(() => {
@@ -377,16 +377,19 @@ defineExpose({showSentence, play, del, hideSentence, nextSentence})
       <div class="titleTranslate" v-if="settingStore.translate">{{ props.article.titleTranslate }}</div>
     </header>
     <div class="article-content" ref="articleWrapperRef">
-      <article :class="settingStore.translate && 'tall'">
+      <article :class="[
+          settingStore.translate && 'tall',
+          settingStore.dictation && 'dictation',
+      ]">
         <div class="section" v-for="(section,indexI) in props.article.sections">
                 <span class="sentence"
                       :class="[
-                          sectionIndex === indexI && sentenceIndex === indexJ && settingStore.dictation
-                          ?'dictation':''
+                          hoverIndex.sectionIndex === indexI &&  hoverIndex.sentenceIndex === indexJ
+                          &&'hover-show'
                       ]"
+                      @contextmenu="e=>onContextMenu(e,sentence.text,indexI,indexJ)"
                       @mouseenter="settingStore.allowWordTip && showSentence(indexI,indexJ)"
                       @mouseleave="hideSentence"
-                      @click="playWordAudio(sentence.text)"
                       v-for="(sentence,indexJ) in section">
                   <span
                       v-for="(word,indexW) in sentence.words"
@@ -402,17 +405,20 @@ defineExpose({showSentence, play, del, hideSentence, nextSentence})
                         ''),
                         (`${indexI}${indexJ}${indexW}` === currentIndex && !isSpace && wrong )?'word-wrong':'',
                         indexW === 0 && `word${indexI}-${indexJ}`
-                        ]"
-                      @click="playWord(word)">
-                    <span class="all-word" v-if="`${indexI}${indexJ}${indexW}` === currentIndex && !isSpace">
+                        ]">
+                    <span class="word-wrap" v-if="`${indexI}${indexJ}${indexW}` === currentIndex && !isSpace">
                       <span class="word-start" v-if="input">{{ input }}</span>
                       <span class="word-end">
                         <span class="wrong" :class="wrong === ' ' && 'bg-wrong'" v-if="wrong">{{ wrong }}</span>
-                        <span class="input" v-else>{{ currentWordString(word, indexI, indexJ) }}</span>
-                        <span>{{ currentWordEnd(word, indexI, indexJ,) }}</span>
+                        <span :class="!word.isSymbol && 'dictation-hide'" v-else>{{ indexWord(word) }}</span>
+                        <span class="dictation-hide">{{ remainderWord(word) }}</span>
                       </span>
+                      <span class="border-bottom" v-if="settingStore.dictation"></span>
                     </span>
-                    <span v-else class="all-word">{{ getAllWord(word, indexI, indexJ, indexW) }}</span>
+                    <span v-else class="word-wrap">
+                      <span :class="!word.isSymbol && 'dictation-hide'">{{ word.word }}</span>
+                      <span class="border-bottom" v-if="settingStore.dictation"></span>
+                    </span>
                     <span
                         v-if="word.nextSpace"
                         class="word-end"
@@ -422,7 +428,6 @@ defineExpose({showSentence, play, del, hideSentence, nextSentence})
                     >
                       <span class="word-space"
                             :class="[
-                            `${indexI}${indexJ}${indexW}`,
                             settingStore.dictation && 'to-bottom',
                             (`${indexI}${indexJ}${indexW}` === currentIndex && isSpace && !wrong ) && 'wait',
                         ]"
@@ -433,10 +438,17 @@ defineExpose({showSentence, play, del, hideSentence, nextSentence})
         </div>
       </article>
       <div class="translate" v-show="settingStore.translate">
-        <template v-for="(v,i) in props.article.sections">
+        <template v-for="(v,indexI) in props.article.sections">
           <div class="row"
-               :class="`translate${i+'-'+j}`"
-               v-for="(item,j) in v">
+               :class="[
+                   `translate${indexI+'-'+indexJ}`,
+                   (sectionIndex>indexI
+                        ?'wrote':
+                        (sectionIndex>=indexI &&sentenceIndex>indexJ)
+                        ?'wrote' :
+                        ''),
+                        ]"
+               v-for="(item,indexJ) in v">
             <span class="space"></span>
             <Transition name="fade">
               <span class="text" v-if="item.translate">{{ item.translate }}</span>
@@ -453,19 +465,17 @@ defineExpose({showSentence, play, del, hideSentence, nextSentence})
 @import "@/assets/css/style";
 
 .wrote {
-  //color: green;
-  color: rgb(22, 163, 74);
+  color: grey;
+  //color: rgb(22, 163, 74);
 }
 
-.word {
-  position: relative;
-}
 
 $article-width: 1000px;
 .typing-article {
   height: 100%;
   width: 100%;
   overflow: auto;
+  color: var(--color-article);
 
   header {
     word-wrap: break-word;
@@ -474,16 +484,15 @@ $article-width: 1000px;
 
     .title {
       text-align: center;
-      font-weight: bold;
-      color: rgba(gray, .8);
       font-size: 2.2rem;
-      font-family: var(--word-font-family);
+      font-family: var(--en-article-family);
     }
 
     .titleTranslate {
       @extend .title;
       font-size: 1.2rem;
-      font-family: unset;
+      font-family: var(--zh-article-family);
+      font-weight: bold;
     }
   }
 
@@ -495,33 +504,71 @@ $article-width: 1000px;
   article {
     font-size: 1.6rem;
     line-height: 1.3;
-    color: gray;
     word-break: keep-all;
     word-wrap: break-word;
     white-space: pre-wrap;
+    font-family: var(--en-article-family);
+
+    &.dictation {
+      .dictation-hide {
+        opacity: 0;
+      }
+
+      .border-bottom {
+        display: inline-block !important;
+      }
+    }
+
+    .wrote, .hover-show {
+      .dictation-hide {
+        opacity: 1 !important;
+      }
+
+      .border-bottom {
+        display: none !important;
+      }
+    }
+
+    .hover-show {
+      background: var(--color-main-active);
+      color: white !important;
+      .wrote{
+        color: white !important;
+      }
+    }
 
     &.tall {
       line-height: 2.5;
     }
 
     .section {
-      margin-bottom: 3rem;
+      margin-bottom: 1.2rem;
 
       .sentence {
         transition: all .3s;
 
         &:first-child {
-          padding-left: .2rem;
-        }
-
-        &.dictation {
-          font-family: var(--word-font-family);
-          letter-spacing: .2rem;
+          padding-left: 3rem;
         }
       }
 
       .word {
         display: inline-block;
+
+        .word-wrap {
+          position: relative;
+
+          .border-bottom {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            top: 0;
+            border-bottom: 2px solid var(--color-article);
+            display: none;
+            transform: translateY(-0.2rem);
+          }
+        }
       }
     }
   }
@@ -534,16 +581,17 @@ $article-width: 1000px;
     height: 100%;
     width: 100%;
     font-size: 1.1rem;
-    color: gray;
     line-height: 3.5;
     letter-spacing: .2rem;
-    //display: none;
+    font-family: var(--zh-article-family);
+    font-weight: bold;
 
     .row {
       position: absolute;
       left: 0;
       width: 100%;
       opacity: 0;
+      transition: all .3s;
 
       .space {
         transition: all .3s;
@@ -554,7 +602,6 @@ $article-width: 1000px;
 
   .word-space {
     position: relative;
-    color: gray;
     display: inline-block;
     width: 0.8rem;
     height: 1.5rem;
@@ -566,14 +613,14 @@ $article-width: 1000px;
     }
 
     &.wait {
-      border-bottom: 2px solid gray;
+      border-bottom: 2px solid var(--color-article);
 
       &::after {
         content: ' ';
         position: absolute;
-        width: .1rem;
+        width: 2px;
         height: .25rem;
-        background: gray;
+        background: var(--color-article);
         bottom: 0;
         right: 0;
       }
@@ -581,15 +628,14 @@ $article-width: 1000px;
       &::before {
         content: ' ';
         position: absolute;
-        width: .1rem;
+        width: 2px;
         height: .26rem;
-        background: gray;
+        background: var(--color-article);
         bottom: 0;
         left: 0;
       }
     }
   }
-
 
   .word-start {
     color: var(--color-main-active);
@@ -607,7 +653,6 @@ $article-width: 1000px;
   .bg-wrong {
     display: inline-block;
     line-height: 1;
-    color: gray;
     background: rgba(red, 0.6);
     animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
   }
@@ -623,7 +668,7 @@ $article-width: 1000px;
 
 @keyframes underline {
   0%, 100% {
-    border-left: .1rem solid black;
+    border-left: .1rem solid var(--color-article);
   }
   50% {
     border-left: .1rem solid transparent;
