3ba825a6e43af1c01f9d0fb6ad646fbd88c06186
zyronon <zyronon@163.com>
2025-07-19 23:44:41 +0800

    feat:删除git issue模板

--- src/components/toolbar/FeedbackModal.vue
+++ src/components/toolbar/FeedbackModal.vue
@@ -20,20 +20,11 @@ const emit = defineEmits([
       <p>or</p>
       <div class="github">
         <span>在<a :href="GITHUB" target="_blank">Github</a>上给我提一个
-        <a :href="`${GITHUB}/issues`" target="_blank">Issue</a>
         </span>
         <div class="options">
           <BaseButton>
-            <a :href="`${GITHUB}/issues/new?assignees=&labels=&projects=&template=%E5%8D%95%E8%AF%8D%E9%94%99%E8%AF%AF---word-error.md&title=%E5%8D%95%E8%AF%8D%E9%94%99%E8%AF%AF+%7C+Word+error`"
-               target="_blank">词典错误？</a>
-          </BaseButton>
-          <BaseButton>
-            <a :href="`${GITHUB}/issues/new?assignees=&labels=&projects=&template=问题报告---bug-report-.md&title=问题报告+%7C+Bug+report+`"
-               target="_blank">反馈BUG？</a>
-          </BaseButton>
-          <BaseButton>
-            <a :href="`${GITHUB}/issues/new?assignees=&labels=&projects=&template=功能请求---feature-request.md&title=功能请求+%7C+Feature+request`"
-               target="_blank">功能请求？</a>
+            <a :href="`${GITHUB}/issues/new`"
+               target="_blank">Issue</a>
           </BaseButton>
         </div>
       </div>
